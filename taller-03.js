38929cf465a3b3a94cca2165a20138a43732ec58
Katy <jdiazk@uninorte.edu.co>
2024-09-06 15:53:28 -0500

    Upgrade the items of the homework

--- taller-03.js
+++ taller-03.js
@@ -1,8 +1,22 @@
 /*Taller 3- Katy Diaz Beltrán - 200142505
 Desarrollo backend */
-
+listavocales=[a,e,i,o,u]
+listaconsonantes=[b,c,d,f,g,h,j,k,l,m,n,ñ,p,q,r,s,t,v,w,x,y,z]
 function deesglosarString(a){
-
+    let cvocales=0
+    let cconsonantes=0
+    for (let index = 0; index < listavocales.length; index++) {
+        if(a==listaconsonantes[index]){
+            cvocales= cvocales+1
+        }
+        
+    }
+    for (let index2 = 0; index2 < listaconsonantes.length; index2++) {
+        if(a== listaconsonantes[index2]){
+            cconsonantes= cconsonantes+1
+        }
+        
+    }
 }
 lista=[1,3,2,4,5,6,7,8,9]
 function twoSum(lista){
