321d1b9af9388570bca8076a25310495252bfbfb
Katy <jdiazk@uninorte.edu.co>
2024-09-06 00:37:35 -0500

    Start the new homework

---
+++ taller-03.js
@@ -0,0 +1,13 @@
+/*Taller 3- Katy Diaz Beltrán - 200142505
+Desarrollo backend */
+
+function deesglosarString(a){
+
+}
+lista=[1,3,2,4,5,6,7,8,9]
+function twoSum(lista){
+
+}
+function conversionRomana(nromano){
+
+}
