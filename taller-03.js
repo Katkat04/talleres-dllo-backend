4dfeb8ca3b65b45b834eff7fc7de0b9273f15d15
Katy <jdiazk@uninorte.edu.co>
2024-09-06 16:10:53 -0500

    update the code

--- taller-03.js
+++ taller-03.js
@@ -1,27 +1,52 @@
 /*Taller 3- Katy Diaz Beltrán - 200142505
 Desarrollo backend */
-listavocales=[a,e,i,o,u]
-listaconsonantes=[b,c,d,f,g,h,j,k,l,m,n,ñ,p,q,r,s,t,v,w,x,y,z]
-function deesglosarString(a){
-    let cvocales=0
-    let cconsonantes=0
-    for (let index = 0; index < listavocales.length; index++) {
-        if(a==listaconsonantes[index]){
-            cvocales= cvocales+1
+let listavocales = ['a', 'e', 'i', 'o', 'u']
+let listaconsonantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+function desglosarString(a ,option) {
+    let cvocales = 0;
+    let cconsonantes = 0;
+
+    for (let index = 0; index < a.length; index++) {
+        let letra = a[index].toLowerCase()
+        if (listavocales.includes(letra)) {
+            cvocales++
+        } else if (listaconsonantes.includes(letra)) {
+            cconsonantes++
         }
-        
     }
-    for (let index2 = 0; index2 < listaconsonantes.length; index2++) {
-        if(a== listaconsonantes[index2]){
-            cconsonantes= cconsonantes+1
-        }
-        
+    if (option== "vocales"){
+        return cvocales
+    }else if(option== "consonantes"){
+        return cconsonantes
     }
 }
-lista=[1,3,2,4,5,6,7,8,9]
-function twoSum(lista){
+desglosarString(a)
+
+listanum=[1,3,2,4,5,6,7,8,9]
+function twoSum(listanum, numero){
+    for (let index = 0; index < listanum.length; index++) {
+        for (let index2 = index + 1; index2 < listanum.length; index2++) {
+            if (nums[index] + listanum[index2] === numero) {
+                return [index, index2];
+            }
+        }
+    }
 
 }
 function conversionRomana(nromano){
-
+    let i = 1
+    let v = 5
+    let x = 10
+    let l = 50 
+    let c = 100
+    let d = 500
+    let m = 1000
+    let contador = 0
+    for (let index = 0; index < nromano.length; index++) {
+        let letra = nromano[index].toLowerCase()
+        if(letra==i){
+            contador= contador+i
+        }
+        
+    }
 }
